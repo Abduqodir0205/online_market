ed900857c6024b09566e0fb24cba188de448a78f
Abduqodir0205 <abduqodirdeveloper@gmail.com>
2024-05-15 16:20:37 +0500

    as

--- lib/prodacts.dart
+++ lib/prodacts.dart
@@ -8,45 +8,45 @@ List<Map<String, dynamic>> products = [
     "category": "Kurtka",
     "title": "Product 1",
     "label": "Label 1",
-    "price": 100.0,
-    "star": 4.5,
-    "imgPath": "assets/images/user.jpg",
+    "price": 125.2,
+    "star": 3.4,
+    "imgPath": "https://images.uzum.uz/cld26bnn7c6qm23jio90/original.jpg",
     "liked": false
   },
   {
     "category": "Futbolka",
     "title": "Product 2",
     "label": "Label 2",
-    "price": 150.0,
+    "price": 55.0,
     "star": 4.2,
-    "imgPath": "assets/images/futbolka.jpg",
+    "imgPath": "https://images.uzum.uz/cp1k9rc0u44tu6dphmn0/original.jpg",
     "liked": false
   },
   {
     "category": "Futbolka",
     "title": "Product 3",
     "label": "Label 1",
-    "price": 100.0,
-    "star": 4.5,
-    "imgPath": "assets/images/futbolka1.jpg",
+    "price": 77.0,
+    "star": 4.0,
+    "imgPath": "https://images.uzum.uz/cglvpvr57mg9720dm71g/original.jpg",
     "liked": false
   },
   {
     "category": "Shim",
     "title": "Product 4",
     "label": "Label 1",
-    "price": 100.0,
+    "price": 120.0,
     "star": 4.5,
-    "imgPath": "assets/images/shim.jpg",
+    "imgPath": "https://images.uzum.uz/cmn1gs925ku8ad8ia0vg/original.jpg",
     "liked": false
   },
   {
     "category": "Ko'ylak",
     "title": "Product 5",
     "label": "Label 1",
-    "price": 100.0,
-    "star": 4.5,
-    "imgPath": "assets/images/shirt.jpg",
+    "price": 90.0,
+    "star": 4.2,
+    "imgPath": "https://images.uzum.uz/cii13frltlh4bk4n3b5g/original.jpg",
     "liked": false
   },
   {
@@ -55,7 +55,7 @@ List<Map<String, dynamic>> products = [
     "label": "Label 1",
     "price": 100.0,
     "star": 4.5,
-    "imgPath": "assets/images/pants.jpg",
+    "imgPath": "https://images.uzum.uz/cotcqss0u44tu6dok5a0/original.jpg",
     "liked": false
   },
   {
@@ -64,7 +64,7 @@ List<Map<String, dynamic>> products = [
     "label": "Label 1",
     "price": 100.0,
     "star": 4.5,
-    "imgPath": "assets/images/bag.jpg",
+    "imgPath": "https://images.uzum.uz/cohm6d21om4pepe0nnqg/original.jpg",
     "liked": false
   },
   {
@@ -73,7 +73,7 @@ List<Map<String, dynamic>> products = [
     "label": "Label 1",
     "price": 50.0,
     "star": 4.5,
-    "imgPath": "assets/images/bag2.jpg",
+    "imgPath": "https://images.uzum.uz/cnjjatg4idugcqegbbp0/original.jpg",
     "liked": false
   },
   {
@@ -82,7 +82,7 @@ List<Map<String, dynamic>> products = [
     "label": "Label 1",
     "price": 100.0,
     "star": 4.5,
-    "imgPath": "assets/images/shirt1.jpg",
+    "imgPath": "https://images.uzum.uz/con4vbbkkckv6jb5jh7g/original.jpg",
     "liked": false
   },
   // Add more products as needed
